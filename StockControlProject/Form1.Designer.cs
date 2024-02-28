85882ccaba7972ad40dbbd85caf41f68419c2a5e
Berkay Serpek <berkayserpek@gmail.com>
2024-02-28 11:02:06 +0300

    Add project files.

---
+++ StockControlProject/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace StockControlProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dtpStartDate = new System.Windows.Forms.DateTimePicker();
+            this.lblStartDate = new System.Windows.Forms.Label();
+            this.lblEndDate = new System.Windows.Forms.Label();
+            this.dtpEndDate = new System.Windows.Forms.DateTimePicker();
+            this.cBoxItemCode = new System.Windows.Forms.ComboBox();
+            this.lblItemCode = new System.Windows.Forms.Label();
+            this.lblItemName = new System.Windows.Forms.Label();
+            this.cBoxItemName = new System.Windows.Forms.ComboBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnImportExcel = new System.Windows.Forms.Button();
+            this.btnPrint = new System.Windows.Forms.Button();
+            this.btnResult = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(356, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(317, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "12M Consulting Stok Takip Sistemi";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::StockControlProject.Properties.Resources.indir;
+            this.pictureBox1.Location = new System.Drawing.Point(850, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(165, 153);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dtpStartDate
+            // 
+            this.dtpStartDate.Location = new System.Drawing.Point(15, 85);
+            this.dtpStartDate.Name = "dtpStartDate";
+            this.dtpStartDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpStartDate.TabIndex = 2;
+            // 
+            // lblStartDate
+            // 
+            this.lblStartDate.AutoSize = true;
+            this.lblStartDate.Location = new System.Drawing.Point(12, 61);
+            this.lblStartDate.Name = "lblStartDate";
+            this.lblStartDate.Size = new System.Drawing.Size(82, 13);
+            this.lblStartDate.TabIndex = 3;
+            this.lblStartDate.Text = "Başlangıç Tarihi";
+            // 
+            // lblEndDate
+            // 
+            this.lblEndDate.AutoSize = true;
+            this.lblEndDate.Location = new System.Drawing.Point(230, 61);
+            this.lblEndDate.Name = "lblEndDate";
+            this.lblEndDate.Size = new System.Drawing.Size(55, 13);
+            this.lblEndDate.TabIndex = 4;
+            this.lblEndDate.Text = "Bitiş Tarihi";
+            // 
+            // dtpEndDate
+            // 
+            this.dtpEndDate.Location = new System.Drawing.Point(233, 85);
+            this.dtpEndDate.Name = "dtpEndDate";
+            this.dtpEndDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpEndDate.TabIndex = 5;
+            // 
+            // cBoxItemCode
+            // 
+            this.cBoxItemCode.FormattingEnabled = true;
+            this.cBoxItemCode.Location = new System.Drawing.Point(449, 85);
+            this.cBoxItemCode.Name = "cBoxItemCode";
+            this.cBoxItemCode.Size = new System.Drawing.Size(155, 21);
+            this.cBoxItemCode.TabIndex = 6;
+            // 
+            // lblItemCode
+            // 
+            this.lblItemCode.AutoSize = true;
+            this.lblItemCode.Location = new System.Drawing.Point(446, 61);
+            this.lblItemCode.Name = "lblItemCode";
+            this.lblItemCode.Size = new System.Drawing.Size(52, 13);
+            this.lblItemCode.TabIndex = 7;
+            this.lblItemCode.Text = "Mal Kodu";
+            // 
+            // lblItemName
+            // 
+            this.lblItemName.AutoSize = true;
+            this.lblItemName.Location = new System.Drawing.Point(607, 61);
+            this.lblItemName.Name = "lblItemName";
+            this.lblItemName.Size = new System.Drawing.Size(42, 13);
+            this.lblItemName.TabIndex = 8;
+            this.lblItemName.Text = "Mal Adı";
+            // 
+            // cBoxItemName
+            // 
+            this.cBoxItemName.FormattingEnabled = true;
+            this.cBoxItemName.Location = new System.Drawing.Point(610, 85);
+            this.cBoxItemName.Name = "cBoxItemName";
+            this.cBoxItemName.Size = new System.Drawing.Size(234, 21);
+            this.cBoxItemName.TabIndex = 9;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(879, 469);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(146, 55);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Temizle";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 165);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(764, 368);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // btnImportExcel
+            // 
+            this.btnImportExcel.Location = new System.Drawing.Point(619, 131);
+            this.btnImportExcel.Name = "btnImportExcel";
+            this.btnImportExcel.Size = new System.Drawing.Size(75, 23);
+            this.btnImportExcel.TabIndex = 12;
+            this.btnImportExcel.Text = "Excele Aktar";
+            this.btnImportExcel.UseVisualStyleBackColor = true;
+            this.btnImportExcel.Click += new System.EventHandler(this.btnImportExcel_Click);
+            // 
+            // btnPrint
+            // 
+            this.btnPrint.Location = new System.Drawing.Point(704, 131);
+            this.btnPrint.Name = "btnPrint";
+            this.btnPrint.Size = new System.Drawing.Size(75, 23);
+            this.btnPrint.TabIndex = 13;
+            this.btnPrint.Text = "Yazdır";
+            this.btnPrint.UseVisualStyleBackColor = true;
+            this.btnPrint.Click += new System.EventHandler(this.btnPrint_Click);
+            // 
+            // btnResult
+            // 
+            this.btnResult.Location = new System.Drawing.Point(15, 131);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(126, 23);
+            this.btnResult.TabIndex = 14;
+            this.btnResult.Text = "Sonuçları Göster";
+            this.btnResult.UseVisualStyleBackColor = true;
+            this.btnResult.Click += new System.EventHandler(this.btnResult_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1037, 545);
+            this.Controls.Add(this.btnResult);
+            this.Controls.Add(this.btnPrint);
+            this.Controls.Add(this.btnImportExcel);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.cBoxItemName);
+            this.Controls.Add(this.lblItemName);
+            this.Controls.Add(this.lblItemCode);
+            this.Controls.Add(this.cBoxItemCode);
+            this.Controls.Add(this.dtpEndDate);
+            this.Controls.Add(this.lblEndDate);
+            this.Controls.Add(this.lblStartDate);
+            this.Controls.Add(this.dtpStartDate);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "12M Consulting Stok Takip Sistemi";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DateTimePicker dtpStartDate;
+        private System.Windows.Forms.Label lblStartDate;
+        private System.Windows.Forms.Label lblEndDate;
+        private System.Windows.Forms.DateTimePicker dtpEndDate;
+        private System.Windows.Forms.ComboBox cBoxItemCode;
+        private System.Windows.Forms.Label lblItemCode;
+        private System.Windows.Forms.Label lblItemName;
+        private System.Windows.Forms.ComboBox cBoxItemName;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnImportExcel;
+        private System.Windows.Forms.Button btnPrint;
+        private System.Windows.Forms.Button btnResult;
+    }
+}
+
---
+++ StockControlProject/Form1.cs
@@ -0,0 +1,360 @@
+﻿using ClosedXML.Excel;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StockControlProject
+{
+    public partial class Form1 : Form
+    {
+        DataTable importExcel = new DataTable();
+        string connectionString = "Server=RABIYAGEZER\\BERKAY;Initial Catalog=Test;User Id=sa;Password=berkay345..";
+        private int rowIndex = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeDropDownItems();
+        }
+
+        private void InitializeDropDownItems()
+        {
+            // Dropdown'ı doldur
+            FillDropDownItems();
+            btnPrint.Visible = false;
+            btnImportExcel.Visible = false;
+            dataGridView1.Visible = false;
+            btnClear.Visible = false;
+
+        }
+
+        private void FillDropDownItems()
+        {
+            try
+            {
+                // Veritabanı bağlantısı
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    // Prosedür çağırma
+                    SqlCommand stkCommand = new SqlCommand("GetUniqueItems", connection);
+                    stkCommand.CommandType = CommandType.StoredProcedure;
+
+                    // Veritabanı bağlantısını açma
+                    connection.Open();
+
+                    // Verileri okuma
+                    SqlDataReader stkReader = stkCommand.ExecuteReader();
+
+                    List<STK> stkNameList = new List<STK>();
+                    List<STK> stkCodeList = new List<STK>();
+
+
+                    stkCodeList.Add(new STK { ID = 0, MalKodu = "Seçiniz", MalAdi = "" });
+                    stkNameList.Add(new STK { ID = 0, MalAdi = "Seçiniz", MalKodu = "" });
+
+                    // Dropdown'u doldurma
+                    while (stkReader.Read())
+                    {
+                        STK stk = new STK();
+                        stk.ID = Convert.ToInt32(stkReader["ID"].ToString());
+                        stk.MalKodu = stkReader["MalKodu"].ToString();
+                        stk.MalAdi = stkReader["MalAdi"].ToString();
+
+                        cBoxItemName.DisplayMember = "MalAdi";
+                        cBoxItemName.ValueMember = "MalKodu";
+
+                        cBoxItemCode.DisplayMember = "MalKodu";
+                        cBoxItemCode.ValueMember = "MalKodu";
+
+                        cBoxItemCode.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+                        cBoxItemCode.AutoCompleteSource = AutoCompleteSource.ListItems;
+
+
+                        cBoxItemName.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+                        cBoxItemName.AutoCompleteSource = AutoCompleteSource.ListItems;
+
+
+                        stkNameList.Add(stk);
+                        stkCodeList.Add(stk);
+
+                    }
+                    cBoxItemCode.DataSource = stkCodeList;
+                    cBoxItemName.DataSource = stkNameList;
+
+                    // Veritabanı bağlantısını kapatma
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                // Hata durumunda kullanıcıya bilgi verme
+                MessageBox.Show("Bir hata oluştu", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnResult_Click(object sender, EventArgs e)
+        {
+            if (CheckValues())
+                GetResult();
+        }
+
+
+        private bool CheckValues()
+        {
+            string itemCode;
+            string itemName;
+            DateTime startDate;
+            DateTime endDate;
+            if (cBoxItemCode.SelectedValue.ToString() != "Seçiniz")
+            {
+                itemCode = cBoxItemCode.SelectedValue.ToString();
+            }
+            if (cBoxItemCode.SelectedValue.ToString() != "Seçiniz")
+            {
+                itemName = cBoxItemName.SelectedValue.ToString();
+            }
+
+            if (cBoxItemCode.SelectedIndex == -1)
+            {
+                cBoxItemCode.SelectedIndex = 0;
+            }
+
+            if (cBoxItemName.SelectedIndex == -1)
+            {
+                cBoxItemName.SelectedIndex = 0;
+            }
+
+            if (cBoxItemCode.SelectedValue.ToString() == "Seçiniz" && cBoxItemName.SelectedValue.ToString() == "")
+            {
+                MessageBox.Show("Lütfen MalKodu veya MalAdı alanlarından bir tanesini seçiniz!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                cBoxItemCode.SelectedValue = "Seçiniz";
+                cBoxItemName.SelectedValue = "";
+                return false;
+
+            }
+
+            startDate = dtpStartDate.Value.Date;
+            endDate = dtpEndDate.Value.Date;
+
+
+            if (startDate > endDate)
+            {
+                MessageBox.Show("Başlangıç tarihi bitiş tarihinden küçük olamaz!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                dtpStartDate.Value = DateTime.Now;
+                dtpEndDate.Value = DateTime.Now;
+                return false;
+            }
+            return true;
+        }
+
+        private void GetResult()
+        {
+            try
+            {
+                // Veritabanı bağlantısını oluştur
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    // Prosedürü çağırma
+                    SqlCommand stiCommand = new SqlCommand("GetStockTransactions", connection);
+                    stiCommand.CommandType = CommandType.StoredProcedure;
+
+                    // Parametreleri belirleme
+                    var startDate = Convert.ToInt32(dtpStartDate.Value.ToOADate());
+                    var endDate = Convert.ToInt32(dtpEndDate.Value.ToOADate());
+
+                    // Prosedür için parametreleri ayarlama
+                    stiCommand.Parameters.AddWithValue("@BaslangicTarihi", startDate);
+                    stiCommand.Parameters.AddWithValue("@BitisTarihi", endDate);
+                    if (cBoxItemCode.SelectedValue != "Seçiniz")
+                    {
+                        stiCommand.Parameters.AddWithValue("@MalKodu", cBoxItemCode.SelectedValue);
+                    }
+                    else
+                    {
+                        stiCommand.Parameters.AddWithValue("@MalKodu", cBoxItemName.SelectedValue);
+                    }
+
+                    // Veritabanı bağlantısını açma
+                    connection.Open();
+
+
+                    // Verileri okuma
+                    SqlDataReader stiReader = stiCommand.ExecuteReader();
+                    // Bir DataTable oluştur
+                    DataTable dataTable = new DataTable();
+
+                    dataTable.Columns.Add("SıraNo", typeof(int));
+                    dataTable.Columns.Add("EvrakNo", typeof(string));
+                    dataTable.Columns.Add("IslemTuru", typeof(string));
+                    dataTable.Columns.Add("Tarih", typeof(DateTime));
+                    dataTable.Columns.Add("GirisMiktar", typeof(decimal));
+                    dataTable.Columns.Add("CikisMiktar", typeof(decimal));
+                    dataTable.Columns.Add("Stok", typeof(decimal));
+
+                    // Stok miktarını hesaplama
+                    decimal stok = 0;
+                    while (stiReader.Read())
+                    {
+                        int siraNo = Convert.ToInt32(stiReader["SiraNo"]);
+                        string evrakNo = stiReader["EvrakNo"].ToString();
+                        string islemTur = stiReader["IslemTur"].ToString();
+                        DateTime tarih = Convert.ToDateTime(stiReader["Tarih"]);
+                        decimal girisMiktar = Convert.ToDecimal(stiReader["GirisMiktar"]);
+                        decimal cikisMiktar = Convert.ToDecimal(stiReader["CikisMiktar"]);
+
+                        if (islemTur == "Giriş") // Giriş işlemi
+                        {
+                            stok += girisMiktar;
+                            dataTable.Rows.Add(siraNo, evrakNo, islemTur, tarih, girisMiktar, 0, stok);
+                        }
+                        else // Çıkış işlemi
+                        {
+                            stok -= cikisMiktar;
+                            dataTable.Rows.Add(siraNo, evrakNo, islemTur, tarih, 0, cikisMiktar, stok);
+                        }
+                    }
+
+                    // SqlDataReader kapat
+                    stiReader.Close();
+
+                    if (dataTable.Rows.Count == 0)
+                    {
+                        MessageBox.Show("Girilen değerlere göre sonuç bulunamadı!", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        dataTable.Columns.Clear();
+                        dataTable.Rows.Clear();
+                        dataGridView1.Visible = false;
+                        btnPrint.Visible = false;
+                        btnImportExcel.Visible = false;
+                        btnClear.Visible = false;
+
+                    }
+
+                    if (dataTable.Rows.Count > 0)
+                    {
+                        dataGridView1.Visible = true;
+
+                        btnImportExcel.Visible = true;
+                        btnPrint.Visible = true;
+                        btnClear.Visible = true;
+                        importExcel = dataTable;
+
+                        // DataGridView'e DataTable'ı atama
+                        dataGridView1.DataSource = dataTable;
+                    }
+
+                    // Veritabanı bağlantısını kapatma
+                    connection.Close();
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                // Hata durumunda kullanıcıya bilgi verme
+                MessageBox.Show("Bir hata oluştu", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            dtpStartDate.Value = DateTime.Now;
+            dtpEndDate.Value = DateTime.Now;
+            cBoxItemCode.SelectedIndex = 0;
+            cBoxItemName.SelectedIndex = 0;
+            btnImportExcel.Visible = false;
+            btnPrint.Visible = false;
+            dataGridView1.Visible = false;
+            btnClear.Visible = false;
+        }
+
+        private void btnImportExcel_Click(object sender, EventArgs e)
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                // Yeni bir çalışma sayfası oluştur
+                var worksheet = workbook.Worksheets.Add("Veriler");
+
+                // DataTable'dan gelen verileri excel sayfasına aktar
+                worksheet.Cell(1, 1).InsertTable(importExcel);
+
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.Filter = "Excel Dosyaları|*.xlsx|Tüm Dosyalar|*.*";
+                saveFileDialog.Title = "Excel Dosyasını Kaydet";
+                saveFileDialog.ShowDialog();
+
+                if (saveFileDialog.FileName != "")
+                {
+                    workbook.SaveAs(saveFileDialog.FileName);
+                }
+            }
+        }
+
+        private void btnPrint_Click(object sender, EventArgs e)
+        {
+            PrintDocument printDocument = new PrintDocument();
+            printDocument.PrintPage += PrintDocument_PrintPage;
+            printDocument.DefaultPageSettings.Landscape = true;
+
+            PrintPreviewDialog printPreviewDialog = new PrintPreviewDialog();
+            printPreviewDialog.Document = printDocument;
+
+            printPreviewDialog.ShowDialog();
+        }
+
+        private void PrintDocument_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Font font = new Font("Arial", 12);
+            Brush brush = Brushes.Black;
+
+            float yPos = 0;
+            float leftMargin = e.MarginBounds.Left;
+            float topMargin = e.MarginBounds.Top;
+            float columnWidth = e.MarginBounds.Width / importExcel.Columns.Count;
+
+
+            //başlıkları atamak istedim fakat tam verimli olmadı.
+            //foreach (DataColumn column in importExcel.Columns)
+            //{
+            //    g.DrawString(column.ColumnName, font, brush, leftMargin, topMargin + yPos);
+            //    leftMargin += columnWidth;
+            //}
+            //yPos += font.GetHeight();
+
+            while (rowIndex < importExcel.Rows.Count)
+            {
+                DataRow row = importExcel.Rows[rowIndex];
+                for (int j = 0; j < importExcel.Columns.Count; j++)
+                {
+                    g.DrawString(row[j].ToString(), font, brush, leftMargin, topMargin + yPos);
+                    leftMargin += columnWidth;
+                }
+                yPos += font.GetHeight();
+                leftMargin = e.MarginBounds.Left;
+                rowIndex++;
+
+                if (yPos + font.GetHeight() > e.MarginBounds.Bottom)
+                {
+                    e.HasMorePages = true;
+                    return;
+                }
+            }
+
+            e.HasMorePages = false;
+            rowIndex = 0;
+        }
+
+        private void btnResult_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
